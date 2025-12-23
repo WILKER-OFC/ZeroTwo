1e1e320cc0ad537b01d7b7ab18c64e2660ce5c52
WILKER OFC <developer.wilker.ofc@gmail.com>
2025-12-23 00:09:42 -0400

    Actualizar bancheck.js

--- plugins/bancheck.js
+++ plugins/bancheck.js
@@ -38,8 +38,7 @@ const handler = async (msg, { conn, text}) => {
 });
 
   try {
-    const url = `https://api.dead.lt/v1/bancheck?number=50768888888,`;
-{
+    const url = `https://api.dead.lt/v1/bancheck?number=50768888888`;
     const res = await fetch(url, {
       headers: {
         Accept: "application/json",
