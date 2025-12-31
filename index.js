639d29e9b01969e42ae74d97baa5d5ce9cb94db4
WILKER OFC <developer.wilker.ofc@gmail.com>
2025-12-30 23:40:48 -0400

    Actualizar index.js

--- index.js
+++ index.js
@@ -33,12 +33,12 @@ const PORT = process.env.PORT || process.env.SERVER_PORT || 5003
 
 let { say } = cfonts
 console.log(chalk.magentaBright('\n❀ Iniciando...'))
-say('Yuki Suou', {
+say('Aiko bot', {
 font: 'simple',
 align: 'left',
 gradient: ['green', 'white']
 })
-say('Made with love by Destroy', {
+say('Made with love by Wilker', {
 font: 'console',
 align: 'center',
 colors: ['cyan', 'magenta', 'yellow']
